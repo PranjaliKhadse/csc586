a577354ceb7513b925cc00e2023ba71b5ce2e823
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-26 17:14:32 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -6,24 +6,24 @@ export DEBIAN_FRONTEND='non-interactive'
 
 echo -e "
 slapd   slapd/password1 password
-slapd   slapd/internal/generated_adminpw        password
-                                                                                                                                
 slapd   slapd/password2 password
+                                                                                                                                
+slapd   slapd/internal/generated_adminpw        password
+slapd   slapd/ppolicy_schema_needs_update       select  abort installation
+slapd   slapd/password_mismatch note
 slapd   slapd/domain    string  clemson.cloudlab.us
+                                                                                                                                
+slapd   slapd/invalid_config    boolean true
+slapd   slapd/backend   select  MDB
+slapd   slapd/no_configuration  boolean false
+slapd   slapd/upgrade_slapcat_failure   error
+                                                                                                                                
 # Do you want the database to be removed when slapd is purged?
 slapd   slapd/purge_database    boolean false
-                                                                                                                                
-slapd   slapd/dump_database_destdir     string  /var/backups/slapd-VERSION
-slapd   slapd/move_old_database boolean true
-# Potentially unsafe slapd access control configuration
+slapd   slapd/dump_database     select  when needed
 slapd   slapd/unsafe_selfwrite_acl      note
                                                                                                                                 
-slapd   slapd/ppolicy_schema_needs_update       select  abort installation
-slapd   slapd/dump_database     select  when needed
-slapd   slapd/password_mismatch note
-slapd   slapd/invalid_config    boolean true
-                                                                                                                                
-slapd   slapd/upgrade_slapcat_failure   error
+slapd   slapd/dump_database_destdir     string  /var/backups/slapd-VERSION
 " | sudo debconf-set-selections
 
 
