eda2f6608508ea938cfbf8a611edf5e586527bca
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-26 17:56:30 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -5,25 +5,28 @@
 export DEBIAN_FRONTEND='non-interactive'
 
 echo -e "
-slapd   slapd/password1 password
-slapd   slapd/password2 password
+slapd   slapd/root_password password  password pkhadse
+slapd   slapd/root_password_again password  password pkhadse
+slapd   slapd/internal/adminpw  password pkhadse
+slapd   slapd/password1 password pkhadse
                                                                                                                                 
-slapd   slapd/internal/generated_adminpw        password
-slapd   slapd/ppolicy_schema_needs_update       select  abort installation
-slapd   slapd/password_mismatch note
-slapd   slapd/domain    string  clemson.cloudlab.us
-                                                                                                                                
-slapd   slapd/invalid_config    boolean true
-slapd   slapd/backend   select  MDB
-slapd   slapd/no_configuration  boolean false
-slapd   slapd/upgrade_slapcat_failure   error
-                                                                                                                                
+slapd   slapd/internal/generated_adminpw        password pkhadse
+slapd   slapd/password2 password pkhadse
 # Do you want the database to be removed when slapd is purged?
 slapd   slapd/purge_database    boolean false
-slapd   slapd/dump_database     select  when needed
-slapd   slapd/unsafe_selfwrite_acl      note
                                                                                                                                 
+# Potentially unsafe slapd access control configuration
+slapd   slapd/unsafe_selfwrite_acl      note
+slapd   slapd/invalid_config    boolean true
 slapd   slapd/dump_database_destdir     string  /var/backups/slapd-VERSION
+                                                                                                                                
+slapd   slapd/move_old_database boolean true
+slapd   shared/organization     string  clemson.cloudlab.us
+slapd   slapd/password_mismatch note
+slapd   slapd/ppolicy_schema_needs_update       select  abort installation
+                                                                                                                                
+slapd   slapd/upgrade_slapcat_failure   error
+slapd   slapd/dump_database     select  when needed
 " | sudo debconf-set-selections
 
 
