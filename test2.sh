35fe87c7f15b5916879d53899fcc16cd93171d42
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-27 18:20:38 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,13 +2,13 @@
 
 Arg1=$1
 re="[a-z][a-z0-9_]+$"
-if [[ $Arg1 =~ $re && $Arg1 == *i* && $Arg1 != * ]]; then
+if [[ ! $Arg1 =~ $re && $Arg1 == *i* && $Arg1 != * ]]; then
 echo "options and arguments ok and running in interactive mode"
 exit 0
-elif [[ $Arg1 == * && $Arg1 =~ $re && $Arg1 != *i* ]]; then
+elif [[ $Arg1 == * && ! $Arg1 =~ $re && $Arg1 != *i* ]]; then
 echo "options and arguments ok"
 echo "Running in non interactive mode"
-elif [[ $Arg1 =~ $re ]]; then
+elif [[ ! $Arg1 =~ $re ]]; then
 echo "options and argument ok running in interactive mode"
 exit 0
 else
@@ -18,7 +18,6 @@ echo "Arguments list of usernames"
 echo "Every username must match regex"
 exit 1
 fi
-
 
 
 
