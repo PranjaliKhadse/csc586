1d6352c710b481db6ac77abfeedf143f82ced53b
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-27 19:18:33 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -10,14 +10,17 @@ echo "Arguments :list of usernames"
 echo "Every username must match this regular expression: '^[a-z][a-z0-9_]*$' "
 exit 0
 elif [[ ! $Arg1 =~ $re && $Arg1 == *i* && $Arg1 != * ]]; then
-echo "options and arguments ok and running in interactive mode"
+echo "Options and arguments ok and running in interactive mode"
 exit 0
 elif [[ $Arg1 == * && ! $Arg1 =~ $re && $Arg1 != *i* ]]; then
 echo "options and arguments ok"
 echo "Running in non interactive mode"
 elif [[ ! $Arg1 =~ $re ]]; then
-echo "options and argument ok running in interactive mode"
+echo "options and argument ok running in non-interactive mode"
 exit 0
+elif [[ ! $Arg1 =~ $re && $Arg1 == *i* && $Arg1 == * ]]; then
+echo "ERR: option after an argument or wrong argument or wrong option 2: -i
+echo "Please provide correct arguments and options"
 else
 echo "Err: option after an argument or wrong option 2:"
 echo "Please provide correct arguments and options"
