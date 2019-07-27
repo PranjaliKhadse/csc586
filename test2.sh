9515f7a50459efaefa1636b0fb9d591cad133002
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-27 18:35:36 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,6 +2,13 @@
 
 Arg1=$1
 re="[a-z][a-z0-9_]+$"
+if [[ $# -eq 0 ]]; then
+echo "Please provide correct arguments and options"
+echo "Usage: ./check_arg.sh[-i][-n][USERS]"
+echo "Options(Optional):-i or -n for interactive or non-interactive mode"
+echo "Arguments :list of usernames"
+echo "Every username must match this regular expression: '^[a-z][a-z0-9_]*$' "
+exit 0
 if [[ ! $Arg1 =~ $re && $Arg1 == *i* && $Arg1 != * ]]; then
 echo "options and arguments ok and running in interactive mode"
 exit 0
