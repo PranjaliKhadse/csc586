5b494c002978557ad8b1ca110679619ba5513076
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-27 19:33:42 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -19,7 +19,7 @@ elif [[ ! $Arg1 =~ $re ]]; then
 echo "options and argument ok running in non-interactive mode"
 exit 0
 elif [[ ! $Arg1 =~ $re && $Arg1 == *i* && $Arg1 == * ]]; then
-echo "ERR: option after an argument or wrong argument or wrong option 2: -i"
+echo "ERR: option after an argument or wrong argument or wrong option 2: "-i""
 echo "Please provide correct arguments and options"
 else
 echo "Err: option after an argument or wrong option 2:"
