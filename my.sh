0da709756f4ac7b3909c7397d4cc86354b06b7e6
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-29 12:53:16 -0400

    Update my.sh

--- my.sh
+++ my.sh
@@ -63,7 +63,7 @@ ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f users.ldif
 
 ldapsearch -x -LLL -b dc=clemson,dc=cloudlab,dc=us 'uid=student' cn gidNumber
 
-#sudo apt-get update
+#sudo apt update
 
 #sudo apt install -y libnss-ldap libpam-ldap ldap-utils
 
