ac60683bba84c7e9e01598413607a0975d2ea779
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-07-29 01:20:20 -0400

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -36,24 +36,21 @@ ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w test -f basedn.ldif
 
 #echo   -n  PASS=$(slappasswd -s rammy) | awk '{print PASS}'
 PASS=$(slappasswd -s rammy)
-#echo 'userPassword: $Pass' >> /local/repository/users.ldif
-#cat users.ldif
-sed 's/userPassword: $PASS/userPassword: $Pass/' /local/repository/users.ldif
-#cat userPassword: $PASS /local/repository/users.ldif
-#cat <<EOF >/local/repository/users.ldif
-#dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
-#objectClass: inetOrgPerson
-#objectClass: posixAccount
-#objectClass: shadowAccount
-#uid: student
-#sn: Ram
-#givenName: Golden
-#cn: student
-#displayName: student
-#uidNumber: 10000
-#gidNumber: 5000
-#userPassword: $PASS
-#gecos: Golden Ram
-#loginShell: /bin/dash
-#homeDirectory: /home/student
-#EOF
+
+cat <<EOF >/local/repository/users.ldif
+dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+uid: student
+sn: Ram
+givenName: Golden
+cn: student
+displayName: student
+uidNumber: 10000
+gidNumber: 5000
+userPassword: $PASS
+gecos: Golden Ram
+loginShell: /bin/dash
+homeDirectory: /home/student
+EOF
