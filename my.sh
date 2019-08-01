cf7ef9aa0e8cedc21e695baa6c550d09d27b8285
PranjaliKhadse <43307750+PranjaliKhadse@users.noreply.github.com>
2019-08-01 18:08:39 -0400

    Update my.sh

--- my.sh
+++ my.sh
@@ -38,7 +38,7 @@ sudo apt-get install -y slapd ldap-utils
 sudo ufw allow ldap 
 
 # Populate LDAP
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w royal -f basedn.ldif
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w royal -f /local/repository/basedn.ldif
 
 # Generate password hash
 PASS=$(slappasswd -s rammy)
@@ -61,7 +61,7 @@ homeDirectory: /home/student
 EOF
 
 # Populate LDAP
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w royal -f users.ldif 
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w royal -f /local/repository/users.ldif 
 
 # Test LDAP
 ldapsearch -x -LLL -b dc=clemson,dc=cloudlab,dc=us 'uid=student' cn gidNumber
